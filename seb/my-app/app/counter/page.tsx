1b4ec20b9fd7ec4f66a05572bc7d6b5fbf39d585
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-08-22 19:40:03 +0900

    learn next.js

---
+++ seb/my-app/app/counter/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useState } from "react"; //useState문
+
+ //클라이언트 렌더링.
+
+export default function Counter() {
+
+    const [value, setValue] = useState(0);
+                         //초기값
+  //카운터의 기능
+  // + > +1
+  const plus = () => {
+    setValue(value + 1);
+  };
+  // - > -1
+  const minus = () => {
+    setValue(value - 1);
+  };
+  // reset > 0
+  const reset = () => {
+    setValue(0);
+  };
+  // 음수 막기
+
+
+
+  return (
+
+    <div>
+      <h1>참을 수 없는 버@튼 : {value}</h1>
+      <button onClick={plus}>+</button>
+      <button onClick={minus}>-</button>
+      <button onClick={reset}>reset</button>
+    </div>
+
+  );
+}
+//onClich : 버튼 누르면 괄호 안에 함수 표시
