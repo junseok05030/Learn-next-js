46be9284e9c1e6a501164a90ce8a7891e5472084
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-08-25 22:05:00 +0900

    http://localhost:3000/counter
    http://localhost:3000/counter please

--- seb/my-app/app/counter/page.tsx
+++ seb/my-app/app/counter/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { useState } from "react"; //useState문
 
- //클라이언트 렌더링.
+//클라이언트 렌더링.
 
 export default function Counter() {
-
-    const [value, setValue] = useState(0);
-
+  const [value, setValue] = useState(0);
+  //초기값
   //카운터의 기능
   // + > +1
   const plus = () => {
@@ -14,7 +13,7 @@ export default function Counter() {
   };
   // - > -1
   const minus = () => {
-    setValue(value - 1);    
+    setValue(value - 1);
   };
   // reset > 0
   const reset = () => {
@@ -22,17 +21,13 @@ export default function Counter() {
   };
   // 음수 막기
 
-
-
   return (
-
     <div>
-      <h1>참을 수 없는 버@튼 : {value}</h1>
+      <h1>오르락내리락 : {value}</h1>
       <button onClick={plus}>+</button>
       <button onClick={minus}>-</button>
       <button onClick={reset}>reset</button>
     </div>
-
   );
 }
-//onClick : 버튼 누르면 괄호 안에 함수 표시
+//onClich : 버튼 누르면 괄호 안에 함수 표시
--- seb/my-app/components/navbar.tsx
+++ seb/my-app/components/navbar.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Navbar() {
-  return (
-    <>
-      <nav>
+export default function Navbar(){
+    return (
+      <>
+        <nav>
         <Link href="/">Home</Link>
         <Link href="/counter">Counter</Link>
         <Link href="/detail">Detail</Link>
